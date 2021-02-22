a0e394a643600e5ea8bec3dce415967779ef18b3
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-02-22 15:43:28 -0800

    first commit

---
+++ w1d1_Algorithm/introDemoCode.js
@@ -0,0 +1,11 @@
+console.log(5 + 5);
+
+const X = 5, Y = 5;
+console.log (X + Y);
+
+//after updating this code it dosen't work because we declare the variable using constant key work "const" it dosen't allow us to change.
+//because of that it shows error.
+ X = 6;
+ Y = 6;
+ console.log (X + Y);
+
