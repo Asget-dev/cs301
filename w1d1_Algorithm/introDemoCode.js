1efe43c888241e37c39ec9ecc7b9ebcc495c0f39
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-02-22 20:14:32 -0800

    edditing txt file

--- w1d1_Algorithm/introDemoCode.js
+++ w1d1_Algorithm/introDemoCode.js
@@ -3,8 +3,8 @@ console.log(5 + 5);
 const X = 5, Y = 5;
 console.log (X + Y);
 
-//after updating this code it dosen't work because we declare the variable using constant key work "const" it dosen't allow us to change.
-//because of that it shows error.
+//after updating the previous code, it dosen't work because we declare the variable using constant key work "const" it dosen't allow us to change it.
+//because of that it shows error when we run it.
  X = 6;
  Y = 6;
  console.log (X + Y);
