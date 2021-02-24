70a2e8ab29d75a6253cff09df233dc63be3164eb
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-02-24 00:03:52 -0800

    Create exercise.js

---
+++ w1d3_Selection_Assignment/exercise.js
@@ -0,0 +1,94 @@
+"use strict";
+const prompt = require("prompt-sync")();
+
+// const todayWether = prompt("What is today weather");
+// if(todayWether=='rainy'){
+//     console.log("Get an umbrella");
+// }
+
+// const num = +prompt("Enter a number between 1 to 10");
+// if(num===7){
+//     console.log('Bingo!');
+// }else{
+//     console.log('Try again')
+// }
+
+// const age = prompt("enter your age");
+// if(age <= 0){
+//     console.log("please enter valid age")
+// }else if(age <= 14){
+//     console.log("you can't drive yer")
+// }else if(age <= 18){
+//     console.log("you can drive under supervision")
+// }else {
+//     console.log("you can drive");
+// }
+
+
+// const num = prompt("Enter number between 1 and 5");
+// if(num===1){
+//     console.log("You entered number 1");
+// }
+// else if(num===2){
+//     console.log("You entered number 2")
+// }else if(num===3){
+//     console.log("You entered number 3")
+// }else if(num===4){
+//     console.log("You entered number 4");
+// }else if(num===5){
+//     console.log("You entered number 5")
+// }
+
+// const num = prompt("Enter number between 1 and 5 ");
+// switch(num){
+//     case '1':
+//         console.log("You entered number 1 ");
+//         break;
+//     case  '2':
+//         console.log("You entered number 2 ");
+//         break;
+//     case '3':
+//         console.log("you entered number 3 ")
+//         break;
+//     case '4':
+//         console.log("you entered number 4 ");
+//         break;
+//     case '5':
+//         console.log("You enter number 5 ")
+// }
+
+
+// let weather = prompt("Please enter weather outside");
+// let temp = prompt("Please enter current temperature");
+
+// if(weather ==='sunny'){
+//     if(temp<80){
+//         console.log("Good day for outdoor running");
+//     }else{
+//         console.log("Better use tread mill at home");
+//     }
+// }
+
+
+// let inputOne = prompt("is salesman is salaried");
+// let inputTwo = prompt("how much is your salary");
+// let inputThree;
+// if(inputOne === 'yes'){
+//     if(inputTwo <300){
+//         console.log("No commission")
+//     }else if(inputTwo <500){
+//         inputThree = inputTwo*0.01;
+//         console.log("Your commision is",inputThree)
+//     }else{
+//         inputThree = inputTwo*0.02;
+//     }
+// }else if(inputOne==='no'){
+//     if(inputTwo<300){
+//         inputThree = inputTwo * 0.02
+//         console.log(inputThree);
+//     }else{
+//         inputThree = inputTwo * 0.03;
+//         console.log(inputThree);
+//     }
+    
+// }
