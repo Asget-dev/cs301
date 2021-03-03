40c33347c6005762b8f30753c6b74518f4e60cae
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-03-03 00:46:07 -0800

    Update calc.js

--- w1d5_Function/calc.js
+++ w1d5_Function/calc.js
@@ -1,16 +1,6 @@
 "use strict";
 
-// let calc = function(callBack,number1,number2){
-//     if(callBack==="add"){
-//         return number1 + number2;
-//     }else if(calcType === "multiply"){
-//         return number1 * number2;
-//     }
-// }
-
-// console.log(calc(2,3,'add'))
-// console.log(calc(2,3,'multiply'))
-
+//fuction expresion
 let add = function(number1, number2){
     return number1 + number2;
 }
@@ -23,8 +13,25 @@ let calc = function(callback,number1, number2){
     return callback(number1, number2);
 }
 
-console.log(calc (2,3, function(number1, number2){
+
+// anonymous function
+
+/* console.log(calc (2,3, function(number1, number2){
     return number1- number2;
-}))
-console.log(calc(add,2,3));
-console.log(calc(multiply,2,3));
+})) */
+
+
+
+//arrow function
+ let division = (number1, number2) =>{
+    return number1 + number2;
+ }
+ let subtraction = (number1, number2) =>{
+     return number1 - number2;
+ }
+ 
+
+console.log(calc(add,9,3));
+console.log(calc(multiply,9,3));
+console.log(calc(division,9,3));
+console.log(calc(subtraction,9,3));
